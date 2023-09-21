7f768cbfe44d9414b459be2e26238b066ae5aaf7
Ashistorq <ashishsinghtarwa@gmail.com>
2023-09-22 01:46:29 +0530

    node

--- index.js
+++ index.js
@@ -4,5 +4,12 @@ const User = require("./database");
 
 
 
+app.set("view engine","ejs");
+
+
+app.get("/",(req,res)=>{
+    res.send("This is working")
+})
+
 
 app.listen(4500,()=>{console.log("this is local host 4500")})
